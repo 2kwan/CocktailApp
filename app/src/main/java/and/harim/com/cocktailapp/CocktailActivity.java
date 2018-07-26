2fd385bab817fb854c357cea688abc80b1356ca6
harim4422 <harim4422@naver.com>
2018-07-26 21:31:16 +0900

    UI 완성중 - 메인, 메뉴 , 맛집 , 칵테일, 순위 완료

---
+++ app/src/main/java/and/harim/com/cocktailapp/CocktailActivity.java
@@ -0,0 +1,13 @@
+package and.harim.com.cocktailapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CocktailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cocktail);
+    }
+}
--- app/src/main/java/down_menu.java
+++
@@ -1,2 +0,0 @@
-public class down_menu {
-}
