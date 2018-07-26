ad0a2fb4007b86a0b901698e41a27ffa72b2ae65
harim4422 <harim4422@naver.com>
2018-07-26 21:47:31 +0900

    UI 완성중 - 메인, 메뉴 , 맛집 , 칵테일, 순위 완료 /07.26 _harim 하단 버튼 제외 메인버튼 화면 전환 연결 완료 /07.26 _harim

--- app/src/main/java/and/harim/com/cocktailapp/Main_UI.java
+++ app/src/main/java/and/harim/com/cocktailapp/Main_UI.java
@@ -11,26 +11,39 @@ public class Main_UI extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__ui);
-    }
-
-    public void click_bar(View view) {
-        Intent bar = new Intent(this,BarActivity.class);
-        startActivity(bar);
-    }
-
-    public void click_cocktail(View view) {
-    }
-
-    public void click_rank(View view) {
-        Intent rank = new Intent(this,RankActivity.class);
-        startActivity(rank);
-    }
-
-    public void click_recipe(View view) {
-    }
+    } //메인 실행시 메소드
 
     public void clcik_menu(View view) {
         Intent menu = new Intent(this,MenuActivity.class);
         startActivity(menu);
-    }
+    }//상단 메뉴 클릭시
+
+    public void click_map(View view) {
+    }//상단 지도 클릭시
+
+    public void click_search(View view) {
+    }//상단 검색 클릭시
+
+    public void click_bar(View view) {
+        Intent bar = new Intent(this,BarActivity.class);
+        startActivity(bar);
+    } //맛집 메뉴 클릭시
+
+    public void click_cocktail(View view) {
+        Intent cocktail = new Intent(this,CocktailActivity.class);
+        startActivity(cocktail);
+    }//칵테일 메뉴 클릭시
+
+    public void click_rank(View view) {
+        Intent rank = new Intent(this,RankActivity.class);
+        startActivity(rank);
+    } //순위 메뉴 클릭시
+
+    public void click_recipe(View view) {
+    }//레시피 메뉴 클릭시
+
+
+
+
+
 }
